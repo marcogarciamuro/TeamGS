6232e9946316dc0625adb2a98be68947cadf80dd
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2022-09-29 12:41:04 -0700

    Increased usablity for mobile, enhanced UI for nba site and chat rooms

--- soccer/forms.py
+++ soccer/forms.py
@@ -1,16 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
 
+
 class TeamSearch(forms.Form):
     team_query = forms.CharField(widget=forms.TextInput(
         attrs={
-            'size': '30', 
-            'placeholder': 'Team Search', 
-            'margin': "auto", 
-            'max-width': "300px",
-            # "class": 'basicAutoComplete',
-            # "data-url": "/domain/teams-autocomplete"
-        }), 
+            'size': '15',
+            'placeholder': 'Team Search',
+            'margin': 'auto',
+            'class': 'form-control'
+        }),
         label=""
     )
-    # style="margin:auto;max-width:300px"
