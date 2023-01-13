98290e5b71cae17ebd3f82d15fbccc2534bcbb2f
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2023-01-13 14:08:32 -0800

    Commented out autocomplete widget and functionality for NBA, and edited news article padding

--- nba/forms.py
+++ nba/forms.py
@@ -3,16 +3,26 @@ from dal import autocomplete
 
 
 class TeamSearch(forms.Form):
-    team_query = forms.CharField(
-        widget=autocomplete.ListSelect2(
-            url='/nba_team_autocomplete/',
-            attrs={
-                'size': 15,
-                'data-placeholder': 'Team Name',
-                'class': 'form-control',
-                'data-selected-html': True,
-                'data-result-html': True,
-            }
-        ),
-        label=''
+    team_query = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'size': '15',
+            'placeholder': 'Team Search',
+            'margin': 'auto',
+            'class': 'form-control'
+        }),
+        label=""
     )
+    # Widget configuation for autocomplete
+    # team_query = forms.CharField(
+    #     widget=autocomplete.ListSelect2(
+    #         url='/nba_team_autocomplete/',
+    #         attrs={
+    #             'size': 15,
+    #             'data-placeholder': 'Team Name',
+    #             'class': 'form-control',
+    #             'data-selected-html': True,
+    #             'data-result-html': True,
+    #         }
+    #     ),
+    #     label=''
+    # )
